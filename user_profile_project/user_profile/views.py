5f8f32ffb550e80e694bcad76ad6cb2ead87a632
Suwan-Aiken <dev@suwansankaja.com>
2025-02-02 06:46:23 +0530

    rendering html pages initialized

--- user_profile_project/user_profile/views.py
+++ user_profile_project/user_profile/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Profile Home Page</h1>')
+    return render(request, 'user_profile/profile.html')
 
 def about(request):
-    return HttpResponse('<h1>Profile About</h1>')
+    return render(request, 'user_profile/about.html')
